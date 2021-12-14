3d2589a71fe2a4b44b0a85472a23b82e86f7c832
Fernando Isidro L <luffynando@gmail.com>
2021-12-13 23:36:43 -0600

    docs: added updated docs for release v1.1.1

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
 export * from './expression-extractor-interface';
+export * from './internal/match-detector';
+export * from './extractors/comprobante32';
+export * from './extractors/comprobante33';
+export * from './extractors/retenciones10';
 export * from './discover-extractor';
