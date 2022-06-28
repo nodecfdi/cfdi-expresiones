38a0090f1142ad04d5888cd2e054dd29fad35926
Fernando Isidro L <luffynando@gmail.com>
2022-06-28 02:33:07 -0500

    fix: format total with empty value input

--- src/extractors/standards/format-total10x6.ts
+++ src/extractors/standards/format-total10x6.ts
@@ -2,6 +2,6 @@ import { toFixed } from '~/utils';
 
 export class FormatTotal10x6 {
     public formatTotal(input: string): string {
-        return toFixed(Number.parseFloat(input), 6).padStart(17, '0');
+        return toFixed(Number.parseFloat(input || '0'), 6).padStart(17, '0');
     }
 }
--- src/extractors/standards/format-total18x6.ts
+++ src/extractors/standards/format-total18x6.ts
@@ -2,7 +2,7 @@ import { rtrim, toFixed } from '~/utils';
 
 export class FormatTotal18x6 {
     public formatTotal(input: string): string {
-        let total = rtrim(toFixed(Number.parseFloat(input), 6), '0');
+        let total = rtrim(toFixed(Number.parseFloat(input || '0'), 6), '0');
         if (total.slice(-1) === '.') {
             total = `${total}0`;
         }
