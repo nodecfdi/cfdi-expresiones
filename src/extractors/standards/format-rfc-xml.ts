610d79552540c4704f0667ba7c9e1e0c66c3d00b
Fernando Isidro L <luffynando@gmail.com>
2022-06-28 00:51:43 -0500

    feat: added standards and common functions with test unit c/u

---
+++ src/extractors/standards/format-rfc-xml.ts
@@ -0,0 +1,7 @@
+import { html_entities } from '~/utils';
+
+export class FormatRfcXml {
+    public formatRfc(rfc: string): string {
+        return html_entities(rfc);
+    }
+}
---
+++ tests/unit/extractors/standards/format-rfc-xml.test.ts
@@ -0,0 +1,14 @@
+import { FormatRfcXml } from '~/extractors/standards/format-rfc-xml';
+
+describe('FormatRfcXml', () => {
+    test.each([
+        ['AAA010101AAA', 'AAA010101AAA'],
+        ['AAAA010101AAA', 'AAAA010101AAA'],
+        ['ÑAAA010101AAA', 'ÑAAA010101AAA'],
+        ['&AAA010101AAA', '&amp;AAA010101AAA']
+    ])('format rfc', (input: string, expected: string) => {
+        const extractor = new FormatRfcXml();
+
+        expect(extractor.formatRfc(input)).toBe(expected);
+    });
+});
