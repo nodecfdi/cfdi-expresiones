9b3615fe687d49ebd8c53fc06c876a98b2193cb9
Fernando Isidro L <luffynando@gmail.com>
2022-07-01 13:54:27 -0500

    test: coverage 100% of code

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -17,13 +17,26 @@ const rtrim = (str: string, chart?: string): string => {
 
 const html_entities = (str: string): string => {
     const tagsToReplace: Record<string, string> = {
-        '&': '&amp;'
+        '"': '&quot;',
+        '&': '&amp;',
+        "'": '&#x27;',
+        '<': '&lt;',
+        // See https://mathiasbynens.be/notes/ambiguous-ampersands: in HTML, the
+        // following is not strictly necessary unless it’s part of a tag or an
+        // unquoted attribute value. We’re only escaping it to support those
+        // situations, and for XML support.
+        '>': '&gt;',
+        // In Internet Explorer ≤ 8, the backtick character can be used
+        // to break out of (un)quoted attribute values or HTML comments.
+        // See http://html5sec.org/#102, http://html5sec.org/#108, and
+        // http://html5sec.org/#133.
+        '`': '&#x60;'
     };
     const replaceTag = (tag: string): string => {
-        return tagsToReplace[tag] || tag;
+        return tagsToReplace[tag];
     };
 
-    return str.replace(/[&<>]/g, replaceTag);
+    return str.replace(/["&'<>`]/g, replaceTag);
 };
 
 export { toFixed, rtrim, html_entities };
